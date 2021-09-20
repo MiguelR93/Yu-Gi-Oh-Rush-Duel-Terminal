62d77f6b983837cc965c4314834b573e92edb8ef
MiguelR93 <miguelromerop93@gmail.com>
2021-09-19 23:56:31 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import random
+
+
+player1 = [ 
+    8000, # "lp"
+    [["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000]], # "deck"
+    [], # "extraDeck"
+    [], # "hand"
+    [], # "gy"
+    [], # "excavated"
+    [], # "fieldSpellCardZone"
+    [], # "leftMonsterCardZone"
+    [], # "centerMonsterCardZone"
+    [], # "rightMonsterCardZone"
+    [], # "leftSTCardZone"
+    [], # "centerleftSTCardZone"
+    [] # "rightleftSTCardZone"
+]    
+player2 = [ 
+    8000, # "lp"
+    [["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000],["001", "Dragón", 4000, 4000]], # "deck"
+    [], # "extraDeck"
+    [], # "hand"
+    [], # "gy"
+    [], # "excavated"
+    [], # "fieldSpellCardZone"
+    [], # "leftMonsterCardZone"
+    [], # "centerMonsterCardZone"
+    [], # "rightMonsterCardZone"
+    [], # "leftSTCardZone"
+    [], # "centerleftSTCardZone"
+    [] # "rightleftSTCardZone"
+]
+players = [player1, player2]
+
+thereIsNoWinner = True
+lifePoint0 = not thereIsNoWinner
+youWin = "YOU WIN!!!"
+youLose = "Y O U  L O S E"
+
+def descontadorAleatorioDeLifePoints():
+    players[random.randint(0,1)][0] -= 1000
+
+def evaluateLifePoints():
+    pass
+
+def run():
+    while thereIsNoWinner == True:
+        # player1[0] -= 1000
+        descontadorAleatorioDeLifePoints()
+        print(f"Tus Lp: {player1[0]}\n\nRival LP: {player2[0]}")
+        if player1[0] <= 0:
+            print(youLose)
+            return lifePoint0
+        elif player2[0] <=0:
+            print(youWin)
+            return lifePoint0
+
+
+if __name__ == '__main__':
+    run()
