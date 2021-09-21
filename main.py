41a4b6f688930d8791345f9566dd6f1ce08f7b79
MiguelR93 <miguelromerop93@gmail.com>
2021-09-21 15:40:36 -0500

    adding dictionary deck to player1

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ import random, os
 
 player1 = [ 
     8000, # 0 = "lp"
-    [["011", "Monstruo", "Dragón", 2000, 4000, 8], ["012", "Monstruo", "Dragón", 4000, 3000, 5], ["013", "Monstruo", "Dragón", 3000, 4000, 4], ["010", "Monstruo", "Trueno", 4000, 2000, 6], ["014", "Monstruo", "Trueno", 1000, 4000, 7], ["015", "Monstruo", "Trueno", 4000, 1000, 2], ["020", "Monstruo", "Trueno", 4000, 2000, 3], ["024", "Monstruo", "Trueno", 1000, 4000, 4], ["025", "Monstruo", "Trueno", 4000, 1000, 1], ['022', "Magia"], ['002', "Magia"], ['002', "Magia"], ['002', "Magia"], ['002', "Magia"], ['002', "Magia"], ['001', "Trampa"], ['001', "Trampa"], ['001', "Trampa"], ['001', "Trampa"], ['001', "Trampa"]], # 1 = "deck"
+    [
+        {'id': '1', 'name': 'Rush Dragon Dragears', 'text': '[Requirement]: You can send the top card of your Deck to the GY.\n[Effect]: This turn, this card can make a second attack during the Battle Phase it destroyed a monster by battle.', 'attribute': 'DARK', 'type': 'Dragon', 'level': '7', 'attack': '2500', 'defense': '1500', 'frontier': '[effect]', 'effect': 'none'}, 
+        {'id': '2', 'name': 'Gravity Press Dragon', 'text': '[Requirement] You can send 1 card from your hand to the GY.\n[Effect] Choose 1 face-up monster your opponent controls. It loses 700 ATK/DEF until the end of this turn.', 'attribute': 'EARTH', 'type': 'Dragon', 'level': '6', 'attack': '1500', 'defense': '1000', 'frontier': '[effect]', 'effect': 'none'}, 
+        {'id': '3', 'name': 'Fire Guardian', 'text': 'The gatekeeper guarding the path to the Fire Dragon Kingdom. Those who cannot withstand its blazing breath are not qualified to go any further.', 'attribute': 'FIRE', 'type': 'Dragon', 'level': '6', 'attack': '2100', 'defense': '400', 'frontier': '[normal]'}, 
+        {'id': '4', 'name': 'Dragon Knight of Darkness', 'text': 'A dragon knight who leads the dark army. His merciless strikes are without equal. The evil sword he wields consumes the souls of his enemies, continuing to grow.', 'attribute': 'DARK', 'type': 'Dragon', 'level': '5', 'attack': '1600', 'defense': '1100', 'frontier': '[normal]'}, 
+        {'id': '5', 'name': 'Dragolite', 'text': 'It came from an underground mineral vein. It keeps fighting using the energy of an unknown ore as a power source. Its super hard hitting blows are simply outstanding!', 'attribute': 'EARTH', 'type': 'Dragon', 'level': '4', 'attack': '1500', 'defense': '0', 'frontier': '[normal]'}, 
+        {'id': '6', 'name': 'Twin-Edge Dragon', 'text': '[Requirement]: You can send 1 card from your hand to the GY.\n[Effect]: This card can make a second attack this turn.', 'attribute': 'LIGHT', 'type': 'Dragon', 'level': '3', 'attack': '1000', 'defense': '0', 'frontier': '[effect]', 'effect': 'none'}, 
+        {'id': '7', 'name': "Dragon's Priestess", 'text': "A young priestess from the bloodline of an ancient dragon. She offers her prayers daily for the peace of her tribe. It's said that her clear eyes can change the future of those they lay upon.", 'attribute': 'WATER', 'type': 'Dragon', 'level': '3', 'attack': '1100', 'defense': '100', 'frontier': '[normal]'}, 
+        {'id': '8', 'name': 'Dragon Bat', 'text': "A mysterious organism, researchers are still split over whether it's a bird or a beast. A flock of these flying together appears sort of draconian, so maybe it's a dragon.", 'attribute': 'DARK', 'type': 'Dragon', 'level': '3', 'attack': '1000', 'defense': '400', 'frontier': '[normal]'}, 
+        {'id': '9', 'name': 'Phoenix Dragon', 'text': '[Requirement]: You can send 1 card from your hand to the GY.\n[Effect]: Add 1 monster (Level 5 or higher Dragon) from your GY to your hand.', 'attribute': 'FIRE', 'type': 'Dragon', 'level': '2', 'attack': '500', 'defense': '500', 'frontier': '[effect]', 'effect': 'none'}, 
+        {'id': '10', 'name': 'Palm-Sized Drago', 'text': "A tiny dragon that loves peace. It's very used to people and will come close if you toss it some nuts. Apparently the ring hanging on its neck has some amazing secret.", 'attribute': 'WIND', 'type': 'Dragon', 'level': '1', 'attack': '0', 'defense': '1400', 'frontier': '[normal]'}, 
+        {'id': '11', 'name': 'Dragonic Pressure', 'text': 'none', 'cardType': 'SPELL', 'icon': 'normal', 'effect': '[Requirement]: Send 3 monsters (Dragon) from your hand to the GY.\n[Effect]: Destroy all monsters on the field. If a monster is destroyed by this effect, you can Special Summon 1 monster (Level 4 or lower Dragon) from your GY to your field in face-up Defense Position.'}, 
+        {'id': '12', 'name': "Fire Dragon's Heatflash", 'text': 'none', 'cardType': 'SPELL', 'icon': 'normal', 'effect': '[Requirement]: If you control a face-up monster (Dragon).\n[Effect]: Destroy 1 Spell/Trap your opponent controls.'}
+    ], # 1 = "deck"
     [], # 2 = "extraDeck"
     [], # 3 = "hand"
     [], # 4 = "gy"
@@ -65,15 +78,19 @@ def duelStatus(): # imprime el estado del duelo: LP, deck, mano, campo, cementer
     print(f"Deck rival: {len(player2[1])}") # deck
     print(f"Cartas en mano rival: {len(player2[3])}") # mano
     print("\nCampo oponente:")
+    # s/t
     for i in player2[-1:-4:-1]:
         print(i,end="   ")
     print("\n")
+    # monstruos
     for i in player2[-4:-7:-1]:
         print(i,end="   ")
     print("\n\nTu campo:")
+    # monstruos
     for i in player1[7:10]:
         print(i,end="   ")
     print("\n")
+    # s/t
     for i in player1[10:13]:
         print(i,end="   ")
     # Tu campo:
@@ -83,7 +100,10 @@ def duelStatus(): # imprime el estado del duelo: LP, deck, mano, campo, cementer
     # imprimiendo la mano:
     print(f"Tu mano: {len(player1[3])}")
     for i,a in enumerate(player1[3]):
-        print(f"{i}: {player1[3][i][1:]}")
+        if 'level' in player1[3][i]:
+            print(f"{i}: {player1[3][i]['name'], player1[3][i]['level']}")
+        else:
+            print(f"{i}: {player1[3][i]['name'], player1[3][i]['cardType'], player1[3][i]['icon']}")
 
 
 def gameStart():
