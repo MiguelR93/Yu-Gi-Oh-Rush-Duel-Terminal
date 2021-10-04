5be70d5e54ec0cf674066c0e8015d805aa5a9f27
MR <miguelromerop93@gmail.com>
2021-10-04 18:55:06 -0500

    modificando función summonLoop para que muestre solamente las zonas disponigles

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ thereIsNoWinner = True
 youWin = "YOU WIN!!!"
 youLose = "Y O U  L O S E"
 TURNSCOUNTER = 0
-def littleSleep(): time.sleep(1.5)
+def littleSleep(): time.sleep(1)
 
 
 def descontadorAleatorioDeLifePoints():
@@ -239,39 +239,57 @@ def summonLoop(summonAMonster, summonKind, position):
         input("Presiona Enter para continuar")
     else:
         while True:
-            monsterZonePosition = int(input("En dónde quieres ponerlo?\n1: zona Izquierda\n2: zona central\n3: zona derecha\n"))
-            print(player1[monsterZonePosition+6])
-            littleSleep
-            if len(player1[monsterZonePosition+6]) != 0:
-                if summonKind == 'tribute Summon':
-                    print("Zona ocupada, elige otra")
-                    littleSleep()
-                    continue
-                else:
-                    ocupado = int(input("Esa zona está ocupada!\nQuieres elegir otra?\n1: sí\n2: no\n"))
-                    if ocupado == 1:
+            try:
+                monstersZones = ["1: zona Izquierda", "2: zona central", "3: zona derecha"]
+                print('En dónde quieres ponerlo?')
+                for i,a in enumerate(player1[7:10]):
+                    if len(a) == 0:
+                        print(monstersZones[i])
+                monsterZonePosition = int(input("Escribe el número a la izquierda: "))
+                if len(player1[monsterZonePosition+6]) != 0:
+                    if summonKind == 'tribute Summon':
+                        print("Zona ocupada, elige otra")
+                        littleSleep()
                         continue
-                    elif ocupado == 2:
-                        break
-            else:
-                player1[3][summonAMonster]['position'], player1[3][summonAMonster]['summonKind'], player1[3][summonAMonster]['summoned this turn?'] = position, summonKind, 'yes'
-                print(player1[3][summonAMonster]) # imprime el estado del monstruo
-                player1[monsterZonePosition+6] = player1[3][summonAMonster] # pone el monstruo de la mano en el campo
-                player1[3].remove(player1[3][summonAMonster]) # quita de la mano al monstruo invocado
-                break
+                    else:
+                        ocupado = int(input("Esa zona está ocupada!\nQuieres elegir otra?\n1: sí\n2: no\n"))
+                        if ocupado == 1:
+                            continue
+                        elif ocupado == 2:
+                            break
+                else:
+                    player1[3][summonAMonster]['position'], player1[3][summonAMonster]['summonKind'], player1[3][summonAMonster]['summoned this turn?'] = position, summonKind, 'yes'
+                    print(player1[3][summonAMonster]) # imprime el estado del monstruo
+                    player1[monsterZonePosition+6] = player1[3][summonAMonster] # pone el monstruo de la mano en el campo
+                    player1[3].remove(player1[3][summonAMonster]) # quita de la mano al monstruo invocado
+                    break
+            except IndexError:
+                print('Valor equivocado')
+                # input()
+                littleSleep()
+                continue
+            except ValueError:
+                print('Debes ingresar un número')
+                littleSleep()
+                continue
+
+
+# def sacrifice2(summonAMonster, position):
+#     pass
+
 
 
 def sacrifice1(summonAMonster, position):
-    summonAMonster
+    # summonAMonster # No recuerdo por qué esto estaba aquí ._.
     # debe mostras los monstruos sacrificables y sus índices. Debe dar la opción para realizar cambios o cancelar la invocación y Confirmar la invocación
     print("Monstruos en campo\n")
     # input()
-    validValues = []
+    # validValues = []
     for i,a in enumerate(player1[7:10]):
         if len(a) != 0:
             print(f"{i}: {a['name']}")
             # input()
-            validValues.append(a)
+            # validValues.append(a)
     try:
         choosed = int(input("Elige el monstruos a sacrificar:"))
     except IndexError:
@@ -308,12 +326,12 @@ def normalSummon(position):
         # elif int(player1[3][summonAMonster]['level']) >= 7:
         #     if ocupiedMonsterZones() < 2:
         #         print("No cuentas con monstruos suficientes")
-        #         input("Presiona Enter para continuar")
+        #         littleSleep()
         #     else:
         #         # mostrar los monstruos a sacrificar
         #         # print("entramos a sacrificar")
         #         # input()
-        #         sacrifice1(summonAMonster)
+                # sacrifice2(summonAMonster, position)
     except IndexError:
         print('Valor equivocado')
         # input()
