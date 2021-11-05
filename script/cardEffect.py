b1fdec511f249bb104b0a9380772050878cd3b12
MR <miguelromerop93@gmail.com>
2021-11-04 23:06:18 -0500

    removing unused modules

--- script/cardEffect.py
+++
@@ -1,5 +0,0 @@
-# class CardEffect():
-#     effect = str
-
-#     def __init__(self, effect):
-#         self.effect = effect
--- script/monsterNormal.py
+++
@@ -1,9 +0,0 @@
-# import script.monster as monster
-
-# # import monster
-
-
-# class MonsterNormal(monster.Monster):
-
-#     def __init__(self, id, name, cardType, attribute, typeMonster, level, attack, defense, frontier, text):
-#         super().__init__(id, name, cardType, attribute, typeMonster, level, attack, defense, frontier, text)
